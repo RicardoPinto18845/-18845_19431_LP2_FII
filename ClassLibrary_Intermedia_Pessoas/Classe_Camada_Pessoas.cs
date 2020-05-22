2cc8c49abbe81059d5e886f5d2c55f2d26297e1b
RicardoPinto18845 <64226609+RicardoPinto18845@users.noreply.github.com>
2020-05-22 16:55:53 +0000

    Add files via upload

---
+++ ClassLibrary_Intermedia_Pessoas/Classe_Camada_Pessoas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_Intermedia_Pessoas
+{
+    class Classe_Camada_Pessoas
+    {
+        public bool lo(int n)
+        {
+            return true;
+        }
+    }
+}
